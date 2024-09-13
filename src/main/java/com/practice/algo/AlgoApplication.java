2d91b54b66b417acee7133aabeed33df7f82c82c
gauravpratapsingh <nikumbgo@gmail.com>
2024-09-13 16:48:35 +0530

    test commit

--- src/main/java/com/practice/algo/AlgoApplication.java
+++ src/main/java/com/practice/algo/AlgoApplication.java
@@ -3,6 +3,9 @@ package com.practice.algo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class AlgoApplication {
 
@@ -16,16 +19,28 @@ public class AlgoApplication {
 				int mid = (u + l) / 2;
 				int m1 = maxSubArray(value, l, mid);
 				int m2 = maxSubArray(value, mid + 1, u);
-				return Math.max(m1, m2);
+				int lmin = calculaemininleft(value);
+				int rmax = calculatemaxeight(value);
+				return Math.max(Math.max(m1, m2), rmax-lmin);
 
 		}
 
     public static void main(String[] args) {
         SpringApplication.run(AlgoApplication.class, args);
 
-        int[] values = new int[] {110,2,3,4,100,8};
+        int[] values = new int[] {-90,0,2,3,4,100,8};
         int result = maxSubArray(values,0, values.length-1);
         System.out.println("The result is " + result);
     }
 
+	static int calculatemaxeight(int[] values)
+	{
+		return Arrays.stream(values).max().getAsInt();
+
+	}
+
+	static int calculaemininleft(int[] values)
+	{
+		return Arrays.stream(values).min().getAsInt();
+	}
 }
